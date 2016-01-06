c968286b87a9f86beeac50201922a80c8b5ca443
buiquocviet <buiquocviet1993@gmail.com>
2016-01-06 10:10:18 +0700

    fix redirect

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -10,7 +10,7 @@ class MicropostsController < ApplicationController
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
-      redirect_to root_url
+      redirect_to home_path
     else
     	@feed_items = []
       render 'static_pages/home'
